dc8ec42868bcfcfba9af8953c6770117c47818d5
nisaapriliani21 <145753218+nisaapriliani21@users.noreply.github.com>
2023-10-02 14:44:59 +0700

    first commit

---
+++ tgs1.dart
@@ -0,0 +1,49 @@
+/* 1.Ruas Lingkaran
+void main() {
+  final phi = 3.14;
+  int r = 6;
+  double luas;
+  print(luas = phi * r * r);
+} */
+
+/* 2.3 Variable
+void main() {
+  String firstName = "Saya";
+  String middleName = "Nisa";
+  String lastName = "Apriliani";
+
+  print('$firstName $middleName $lastName');
+}
+ */
+
+/* 3. a Faktorial 10
+void main() {
+  int hasil, i;
+
+  hasil = 1;
+  for (i = 1; i <= 10; i++) {
+    hasil = hasil * i;
+  }
+  print(hasil);
+} */
+
+/* b Faktorial 20
+void main() {
+  int hasil, i;
+
+  hasil = 1;
+  for (i = 1; i <= 20; i++) {
+    hasil = hasil * i;
+  }
+  print(hasil);
+} */
+
+void main() {
+  int hasil, i;
+
+  hasil = 1;
+  for (i = 1; i <= 30; i++) {
+    hasil = hasil * i;
+  }
+  print(hasil);
+}
